a6519f2f4ea93314be5905349642272b7332cf37
Amit Garu <amitgaru2@gmail.com>
2024-10-19 09:04:01 -0600

    updates

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Hello World!",
-        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
-        "tags": [],
-        "url": "/2024/10/18/hello-world.html",
-        "teaser": null
-      }]
